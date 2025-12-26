0f1b4e4d565d74677df04003363ed6d0a8e07514
abdelrahmanmo7amed83-art <abdelrahmanmo7amed83@icloud.com>
2025-12-26 11:28:55 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,19 +31,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/3.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/3.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/3.png",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "3.png",
   },
 }
 
